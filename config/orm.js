45d028b3fd6c2da909dc13b25f0e5bb24b45e5d3
Guillermo Lara <grlara9@gmail.com>
2019-09-25 11:54:45 -0700

    Initialized directory structure

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+var connection = require(./connection.js);
+
+var orm = {
+
+    selectAll(){
+        
+}
+
+    insertOne(){
+    
+}
+
+    updateOne(){
+    
+}
+}
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("JUMBO JACK", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("JR BACON", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("ULTIMATE CHEESE BURGER", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("FAMOUSE STAR", FALSE);
